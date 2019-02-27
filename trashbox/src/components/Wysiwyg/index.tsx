84c7e62bc7ba2d06ba0bc8bdc3a3dd4baed0362f
Mushus <mushus.wynd@gmail.com>
2019-02-27 10:09:43 +0900

    こみっと

---
+++ trashbox/src/components/Wysiwyg/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+const Wysiwig = () => {
+	const [text, setText] = useState('text');
+	return <div />;
+};
+
+export default Wysiwig;
---
+++ trashbox/src/containers/app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Wysiwyg from '~/components/Wysiwyg';
+
+const App = () => (
+	<div>
+		<Wysiwyg />
+	</div>
+);
+
+export default App;
